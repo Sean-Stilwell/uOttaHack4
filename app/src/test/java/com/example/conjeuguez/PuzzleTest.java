9b3731fcaf180e7f09f35a0a325955de7e7ddcc4
Sean-Stilwell <sstil051@uottawa.ca>
2021-02-06 09:39:45 -0500

    Added basic puzzle functionality and reading a CSV for the verbs

---
+++ app/src/test/java/com/example/conjeuguez/PuzzleTest.java
@@ -0,0 +1,18 @@
+package com.example.conjeuguez;
+
+import junit.framework.TestCase;
+
+public class PuzzleTest extends TestCase {
+
+    public void testClass(){
+    }
+
+    public void testGetPronoun() {
+    }
+
+    public void testGetVerb() {
+    }
+
+    public void testGetAnswer() {
+    }
+}
